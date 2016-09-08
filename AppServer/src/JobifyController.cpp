0325d397cb2092595ef3b98873cc0717088fd73d
Javier <javschulz@gmail.com>
2016-09-08 01:25:10 -0300

    MyController turns into Jobify controller, and it's extracted from main.cpp and written in it's own files.

---
+++ AppServer/src/JobifyController.cpp
@@ -0,0 +1,130 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <mongoose/JsonController.h>
+#include <fstream>
+#include <iostream>
+#include "JobifyController.h"
+
+
+
+	int JobifyController::jsonContainsValue(const Json::Value& root, string key, string value) {
+		const Json::Value& users = root["users"];
+		for (Json::ValueConstIterator it = users.begin(); it != users.end();
+				++it) {
+			const Json::Value& user = *it;
+			if (value.compare(user[key].asString()) == 0)
+			{
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+
+	void JobifyController::fillResponse(JsonResponse &response, JsonResponse &jResponse, int code) {
+		response.setCode(code);
+		response["data"] = jResponse;
+		response.setHeader("Content-Type", "application/json; charset=utf-8");
+	}
+
+
+	void JobifyController::registerUser(Request &request, JsonResponse &response) {
+
+		Json::Value users;
+		Json::Reader reader;
+
+		std::ifstream file("users.json", std::ifstream::binary);
+		bool parsingSuccessful2 = reader.parse(file, users, true); //parse process
+		if (!parsingSuccessful2) {
+			std::cout << "Failed to parse"
+					<< reader.getFormattedErrorMessages();
+
+		}
+		std::string data = request.getData();
+
+		Json::Value root;
+
+		Json::Reader reader2;
+		bool parsingSuccessful = reader2.parse(data.c_str(), root); //parse process
+		if (!parsingSuccessful) {
+			std::cout << "Failed to parse"
+					<< reader2.getFormattedErrorMessages();
+
+		}
+		string username = root.get("email", "").asString();
+		string password = root.get("password", "").asString();
+
+		Json::Value user1;
+		user1["email"] = username;
+		user1["password"] = password;
+
+		file.close();
+
+		if (jsonContainsValue(users,"email" ,username) == 1) { //  == 0 && password.compare(user1["pass"].asString()) == 0) {
+			users["users"].append(user1);
+			Json::StyledStreamWriter writer;
+			std::ofstream out("users.json");
+			writer.write(out, users);
+			out.close();
+			response["result"].append("Registration Ok");
+		} else {
+			response["result"].append("The email provided is already registered");
+		}
+	}
+
+	void JobifyController::login(Request &request, JsonResponse &response) {
+		/*		Json::Value users;
+		 Json::Value user1;
+		 Json::Value user2;
+		 user1["login"] = "locolope";
+		 user1["pass"] = "123456";
+		 user2["login"] = "lima";
+		 user2["pass"] = "qwerty";
+		 users.append(user1);
+		 users.append(user2);
+		 */
+		Json::Value users;
+		Json::Reader reader;
+
+		std::ifstream file("users.json", std::ifstream::binary);
+		//file >> users;
+
+		bool parsingSuccessful2 = reader.parse(file, users, true); //parse process
+		if (!parsingSuccessful2) {
+			std::cout << "Failed to parse"
+					<< reader.getFormattedErrorMessages();
+
+		}
+
+		std::string data = request.getData();
+
+		Json::Value root;
+
+		Json::Reader reader2;
+		bool parsingSuccessful = reader2.parse(data.c_str(), root); //parse process
+		if (!parsingSuccessful) {
+			std::cout << "Failed to parse"
+					<< reader2.getFormattedErrorMessages();
+
+		}
+		string username =
+				root.get("email", "A Default Value if not exists").asString();
+
+		string password = root.get("password", "").asString();
+
+		if (jsonContainsValue(users,"email", username) == 0 && jsonContainsValue(users,"password", password) == 0) {
+			response["result"].append("login Ok");
+		} else {
+			response["result"].append("login Failed");
+		}
+	}
+
+	void JobifyController::setup() {
+		// Example of prefix, putting all the urls into "/api"
+		setPrefix("/api");
+
+		addRouteResponse("GET", "/login", JobifyController, login, JsonResponse);
+		addRouteResponse("POST", "/registerUser", JobifyController, registerUser,
+				JsonResponse);
+	}
+
--- AppServer/src/main.cpp
+++ AppServer/src/main.cpp
@@ -2,128 +2,11 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <mongoose/Server.h>
-#include <mongoose/JsonController.h>
-#include <fstream>
-#include <iostream>
+#include "JobifyController.h"
 
 using namespace std;
 using namespace Mongoose;
 
-class MyController: public JsonController {
-public:
-	int jsonContainsValue(const Json::Value& root, string value) {
-		const Json::Value& users = root["users"];
-		for (Json::ValueConstIterator it = users.begin(); it != users.end();
-				++it) {
-			const Json::Value& user = *it;
-			if (value.compare(user["email"].asString()) == 0)
-			{
-				return 0;
-			}
-		}
-		return 1;
-	}
-
-	void registerUser(Request &request, JsonResponse &response) {
-
-		Json::Value users;
-		Json::Reader reader;
-
-		std::ifstream file("users.json", std::ifstream::binary);
-		bool parsingSuccessful2 = reader.parse(file, users, true); //parse process
-		if (!parsingSuccessful2) {
-			std::cout << "Failed to parse"
-					<< reader.getFormattedErrorMessages();
-
-		}
-		std::string data = request.getData();
-
-		Json::Value root;
-
-		Json::Reader reader2;
-		bool parsingSuccessful = reader2.parse(data.c_str(), root); //parse process
-		if (!parsingSuccessful) {
-			std::cout << "Failed to parse"
-					<< reader2.getFormattedErrorMessages();
-
-		}
-		string username = root.get("email", "").asString();
-		string password = root.get("password", "").asString();
-
-		Json::Value user1;
-		user1["email"] = username;
-		user1["password"] = password;
-
-		file.close();
-
-		if (jsonContainsValue(users, username) == 1) { //  == 0 && password.compare(user1["pass"].asString()) == 0) {
-			users["users"].append(user1);
-			Json::StyledStreamWriter writer;
-			std::ofstream out("users.json");
-			writer.write(out, users);
-			out.close();
-			response["result"].append("Registration Ok");
-		} else {
-			response["result"].append("El usuario ya existe.");
-		}
-	}
-
-	void login(Request &request, JsonResponse &response) {
-		/*		Json::Value users;
-		 Json::Value user1;
-		 Json::Value user2;
-		 user1["login"] = "locolope";
-		 user1["pass"] = "123456";
-		 user2["login"] = "lima";
-		 user2["pass"] = "qwerty";
-		 users.append(user1);
-		 users.append(user2);
-		 */
-		Json::Value users;
-		Json::Reader reader;
-
-		std::ifstream file("users.json", std::ifstream::binary);
-		//file >> users;
-
-		bool parsingSuccessful2 = reader.parse(file, users, true); //parse process
-		if (!parsingSuccessful2) {
-			std::cout << "Failed to parse"
-					<< reader.getFormattedErrorMessages();
-
-		}
-
-		std::string data = request.getData();
-
-		Json::Value root;
-
-		Json::Reader reader2;
-		bool parsingSuccessful = reader2.parse(data.c_str(), root); //parse process
-		if (!parsingSuccessful) {
-			std::cout << "Failed to parse"
-					<< reader2.getFormattedErrorMessages();
-
-		}
-		string username =
-				root.get("email", "A Default Value if not exists").asString();
-
-		string password = root.get("password", "").asString();
-
-		if (jsonContainsValue(users, username) == 0) { //  == 0 && password.compare(user1["pass"].asString()) == 0) {
-			response["result"].append("login Ok");
-		} else {
-			response["result"].append("login Failed");
-		}
-	}
-
-	void setup() {
-		// Example of prefix, putting all the urls into "/api"
-		setPrefix("/api");
-
-		addRouteResponse("GET", "/login", MyController, login, JsonResponse);
-		addRouteResponse("POST", "/registerUser", MyController, registerUser,
-				JsonResponse);
-	}
-};
 
 volatile static bool running = true;
 
@@ -139,14 +22,14 @@ int main() {
 
 	signal(SIGINT, handle_signal);
 
-	MyController myController;
+	JobifyController jController;
 	Server server(8080);
-	server.registerController(&myController);
+	server.registerController(&jController);
 	server.setOption("enable_directory_listing", "false");
 	server.start();
 
 	cout << "Server started, routes:" << endl;
-	myController.dumpRoutes();
+	jController.dumpRoutes();
 
 	while (running) {
 		sleep(10);
