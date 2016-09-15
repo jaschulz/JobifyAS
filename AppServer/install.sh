25aa998a0728fa4e93b42d76a411c100290d24c4
Javier <javschulz@gmail.com>
2016-09-15 01:18:43 -0300

    install.sh and .travis.yml modified.

--- AppServer/install.sh
+++ AppServer/install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 root_dir=`pwd`
+install_dir="$root_dir/temp_install"
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update -qq
-sudo apt-get install -qq g++-4.8
-export CXX="g++-4.8"
+# Instalación de dependencias y herramientas
+echo "Installing tools and dependencies"
+apt-get update
+apt-get install -y --assume-yes --force-yes libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev cmake libcppunit-doc libcppunit-dev lcov libboost-all-dev libssl-dev git
 
+## Instala las librerías
+echo "Installing libraries"
+apt-get install -y wget
+mkdir -p temp_install && \
+cd temp_install && \
+
+cd $install_dir
 echo "Installing jsoncpp"
 wget https://github.com/open-source-parsers/jsoncpp/archive/0.y.z.zip && \
 unzip 0.y.z.zip && \
@@ -18,6 +26,7 @@ ar rvs libjsoncpp.a jsoncpp.o && \
 sudo cp libjsoncpp.a /usr/lib && \
 
 
+cd $install_dir
 # Last release does not have cmake enabled, so we install it from master.
 echo "Installing mongose-cpp"
 wget https://github.com/Gregwar/mongoose-cpp/archive/master.zip && \
@@ -27,10 +36,10 @@ cd mongoose-cpp-master && \
 cmake . && \
 make install && \
 
-sudo apt-get install libcurl4-openssl-dev
 
 cd $root_dir
+rm -rf temp_install
+
 echo "Make and install"
 mkdir -p build && cd build && \
-cmake .. 
-make
+cmake .. && make
