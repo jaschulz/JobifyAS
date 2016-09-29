3da789c5819a528d3b17b8fbd4677d73ce601e18
Javier <javier@UltraSamsung.(none)>
2016-09-29 12:34:48 -0300

    Refactoring.

---
+++ AppServer/src/db/dbCredentials.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "dbCredentials.h"
+#include "../utils/Encrypt.h"
+
+#include "leveldb/db.h"
+
+using namespace std;
+
+string dbCredentials::addNewUser(Json::Value &user){
+	leveldb::WriteOptions writeOptions;
+	string username = user.get("email", "").asString();
+	string password = user.get("password", "").asString();
+
+	string pass;
+	string error = "";
+	leveldb::Status st =  db->Get(leveldb::ReadOptions(),username,&pass);
+	if (st.ok() != 1) {
+		if (st.ToString().compare("NotFound: ") == 0) {
+		        db->Put(writeOptions, username, password);    
+		} else {
+			error = "Failed: " + st.ToString();
+		}
+	} else {
+		error = "Email already exists.";
+	}
+	return error;
+}
+
+//verifyLogin
+string dbCredentials::verifyLogin(Json::Value &user){
+
+	string username = user.get("email", "").asString();
+	string password = user.get("password", "").asString();
+	string pass;
+	leveldb::Status st =  db->Get(leveldb::ReadOptions(),username,&pass);
+        string error = "";
+	if (password.compare(pass) != 0) {    
+		error = "Login failed";
+	}
+	return error;
+}
+
+/*Json::Value dbController::getProfile(string &email){
+	string username = user.get("email", "").asString();
+	string password = user.get("password", "").asString();
+	string pass;
+	leveldb::Status st =  db->Get(leveldb::ReadOptions(),username,&pass);
+        string error = "";
+	if (password.compare(pass) != 0) {    
+		error = "Login failed";
+	}
+	return error;
+}*/
+
+string dbCredentials::printDB(){
+
+
+    // Iterate over each item in the database and print them
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    
+    for (it->SeekToFirst(); it->Valid(); it->Next())
+    {
+        cout << it->key().ToString() << " : " << it->value().ToString() << endl;
+    }
+    string error ="";
+    if (false == it->status().ok())
+    {
+        cerr << "An error was found during the scan" << endl;
+        cerr << it->status().ToString() << endl; 
+	error = "Failed: " + it->status().ToString();
+    }
+	delete it;
+	return error;
+}
+
