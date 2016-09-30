a85232fa9b01c9dea7db895f56d1cfa4cb18ee53
Javier <javschulz@gmail.com>
2016-09-30 01:32:49 -0300

    Modified .travis.yml to test in Travis

--- AppServer/src/db/dbUsers.cpp
+++ AppServer/src/db/dbUsers.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 string dbUsers::editProfile(Json::Value &user){
+	leveldb::WriteOptions writeOptions;
 	string username = user.get("email", "").asString();
 	string strJson;
 	leveldb::Status st =  db->Get(leveldb::ReadOptions(),username,&strJson);
@@ -20,5 +21,6 @@ string dbUsers::editProfile(Json::Value &user){
 	string lName = root.get("last_name", "").asString();
 	root["first_name"] = user.get("first_name", fName).asString();
 	root["last_name"] = user.get("last_name", lName).asString();
+	db->Put(writeOptions, username, root.toStyledString());
 	return error;
 }
