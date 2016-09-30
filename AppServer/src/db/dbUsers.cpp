008fece47ac6afd5e46d8002df3797ca07ae0d01
Javier <javschulz@gmail.com>
2016-09-30 01:18:50 -0300

    Modified .travis.yml to test in Travis

--- AppServer/src/db/dbUsers.cpp
+++ AppServer/src/db/dbUsers.cpp
@@ -9,11 +9,16 @@ string dbUsers::editProfile(Json::Value &user){
 	string strJson;
 	leveldb::Status st =  db->Get(leveldb::ReadOptions(),username,&strJson);
         string error = "";
+	Json::Value root;   
 	Json::Reader reader;
-	bool parsingSuccessful = reader.parse( strJson.c_str(), user );     //parse process
+	bool parsingSuccessful = reader.parse( strJson.c_str(), root );     //parse process
 	if ( !parsingSuccessful )
 	{
 		error = reader.getFormattedErrorMessages();;
 	}
+	string fName = root.get("first_name", "").asString();
+	string lName = root.get("last_name", "").asString();
+	root["first_name"] = user.get("first_name", fName).asString();
+	root["last_name"] = user.get("last_name", lName).asString();
 	return error;
 }
