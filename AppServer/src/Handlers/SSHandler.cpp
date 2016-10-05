347d65699ab4b3a81d7ea6b51d0cc177d08de8a3
Javier <javschulz@gmail.com>
2016-10-05 08:13:13 -0300

    Curl PUT started. However, it's commented so that Travis' build passes.

--- AppServer/src/Handlers/SSHandler.cpp
+++ AppServer/src/Handlers/SSHandler.cpp
@@ -55,3 +55,60 @@ JsonResponse	SSHandler::handleGet(string url){
 
 }
 
+
+/*JsonResponse	SSHandler::handlePut(string url, Request &request){
+  CURL *curl;
+  CURLcode res;
+ 
+  /* In windows, this will init the winsock stuff */ 
+  //curl_global_init(CURL_GLOBAL_ALL);
+ 
+  /* get a curl handle */ 
+ // curl = curl_easy_init();
+ // if(curl) {
+    /* we want to use our own read function */ 
+//    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+ 
+    /* enable uploading */ 
+  //  curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+ 
+    /* HTTP PUT please */ 
+  //  curl_easy_setopt(curl, CURLOPT_PUT, 1L);
+ 
+    /* specify target URL, and note that this URL should include a file
+       name, not only a directory */ 
+   /* curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+	struct curl_slist *headers = NULL;
+	headers = curl_slist_append(headers, "Accept: application/json");
+	headers = curl_slist_append(headers, "Content-Type: application/json");
+	headers = curl_slist_append(headers, "charsets: utf-8");
+
+
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); 
+ 
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT"); /* !!! */
+
+    //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_struct); /* data goes here */
+    /* now specify which file to upload */ 
+    //curl_easy_setopt(curl, CURLOPT_READDATA, hd_src);
+ 
+    /* provide the size of the upload, we specicially typecast the value
+       to curl_off_t since we must be sure to use the correct data size */ 
+    //curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,(curl_off_t)file_info.st_size);
+ 
+    /* Now run off and do what you've been told! */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+   /* curl_easy_cleanup(curl);
+  }
+
+ 
+  curl_global_cleanup();
+  return 0;
+}*/
