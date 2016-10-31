217e73db0cf14867c57d6ecfe4114fb5d6a8f008
Javier <javschulz@gmail.com>
2016-10-31 00:39:06 -0300

    New tests + code coverage.

--- AppServer/tests/ProfileTest.h
+++ AppServer/tests/ProfileTest.h
@@ -9,12 +9,15 @@ using namespace CPPUNIT_NS;
 class ProfileTest : public TestFixture {
 CPPUNIT_TEST_SUITE(ProfileTest);
         CPPUNIT_TEST(getEmail);
+        CPPUNIT_TEST(checkPassword);
     CPPUNIT_TEST_SUITE_END();
 
 public:
     ProfileTest();
 
     void getEmail();
+
+	void checkPassword();
 
     virtual void setUp() override;
 
