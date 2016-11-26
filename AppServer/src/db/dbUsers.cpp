f033ce865a8dd885fe334b04a34ed215ce16b0e4
jaschulz <javschulz@gmail.com>
2016-11-26 09:05:32 -0300

    Changes regarding ranking.

--- AppServer/src/db/dbUsers.cpp
+++ AppServer/src/db/dbUsers.cpp
@@ -124,13 +124,12 @@ std::list<Profile> dbUsers::getMostPopularUsers(Json::Value &filter,
 				return profiles;
 			} else {
 				Profile profile(userProfile);
-				string job_position = filter.get("job_positon", profile.getJobPosition()).asString();
-				cout<<"job_positon: "<<job_position<<endl;
+				string job_position = filter.get("job_position", profile.getJobPosition()).asString();
+				cout<<"job_position: "<<job_position<<endl;
 				cout<<"profile.getJobPosition(): "<<profile.getJobPosition()<<endl;
 				string skill = filter.get("skill", "").asString();
 				cout<<"skill: "<<skill<<endl;
-				//TODO tener en cuenta la skill
-				bool hasSkill = false;
+				bool hasSkill = true;
 				if (skill != "") {
 					cout<<"skill !=: "<<skill<<endl;
 					hasSkill = utils::setContainsValue(profile.getSkills(),skill);
